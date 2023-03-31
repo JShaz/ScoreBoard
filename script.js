a867585cf080028321f857c7fc7393ed8e1ee978
JShaz <jamaalshazier92@gmail.com>
2023-03-31 15:43:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+let homeTm = document.getElementById("home")
+let awayTm = document.getElementById("away")
+let homePoints = 0
+let awayPoints = 0
+
+function homeOne() {
+    
+    homePoints += 01;
+    homeTm.innerText = homePoints
+    
+}
+
+function homeTwo() {
+    
+    homePoints += 02;
+    homeTm.innerText = homePoints
+    
+}
+
+function homeThree() {
+    homePoints += 03;
+    homeTm.innerText = homePoints
+    
+}
+
+function awayOne() {
+    awayPoints += 01;
+    awayTm.innerText = awayPoints
+    
+}
+
+function awayTwo() {
+    awayPoints += 02;
+    awayTm.innerText = awayPoints
+    
+}
+
+function awayThree() {
+   awayPoints += 03;
+    awayTm.innerText = awayPoints
+    
+}
+
+
+
+
+
+
+
+
+
